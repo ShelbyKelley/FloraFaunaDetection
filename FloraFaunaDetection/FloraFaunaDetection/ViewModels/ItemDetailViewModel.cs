8fd4a5c3e27683f8d558f6fffc26533ef56d5c70
Shelby Kelley <Shelby@Shelbys-MacBook-Pro-3.local>
2020-06-28 18:49:42 -0400

    initial commit

---
+++ FloraFaunaDetection/FloraFaunaDetection/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using FloraFaunaDetection.Models;
+
+namespace FloraFaunaDetection.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
